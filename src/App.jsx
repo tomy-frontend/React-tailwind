92e76763c64585cd2790f858a101394d51d5645b
tomy.dev <xxx.d1231@gmail.com>
2024-11-28 11:14:50 +0900

    React習熟度更新

--- src/App.jsx
+++ src/App.jsx
@@ -134,9 +134,9 @@ function App() {
               <div className="shadow bg-green-100 mt-2 w-full">
                 <div
                   className="bg-green-600 text-xs leading-none py-1 text-center text-white"
-                  style={{ width: "40%" }}
+                  style={{ width: "50%" }}
                 >
-                  40%
+                  50%
                 </div>
               </div>
             </div>
