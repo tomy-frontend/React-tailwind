3c83930e89511fee9574a25d175c0c24a48c49ac
tomy.dev <xxx.d1231@gmail.com>
2024-12-01 09:10:18 +0900

    copyrights修正

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => (
   <footer className="text-gray-700 bg-gray-100">
     <div className="container px-5 py-12 mx-auto">
       <p className="text-sm text-center">
-        &copy; tomy_dev. All rights reserved.
+        &copy; TommyReactBlog. All rights reserved.
       </p>
     </div>
   </footer>
